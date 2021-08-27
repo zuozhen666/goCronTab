9f1eeb08e603e31bdea7f05a02dbad782a551f83
zuozhen.zuo666 <zuozhen.zuo666@bytedance.com>
2021-08-27 12:57:32 +0800

    aa
    Change-Id: I44fb5df6a8fb44bf196dd425bd9c69d6bb05301d

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/goCronTab
+
+go 1.15
---
+++ master/Config.go
@@ -0,0 +1,39 @@
+package master
+
+import (
+	"encoding/json"
+	"io/ioutil"
+)
+
+// 程序配置
+type Config struct {
+	ApiPort         int `json:"apiPort"`
+	ApiReadTimeout  int `json:"apiReadTimeout"`
+	ApiWriteTimeout int `json:"apiWriteTimeout"`
+}
+
+var (
+	// 单例
+	G_config *Config
+)
+
+func InitConfig(filename string) (err error){
+	var (
+		content []byte
+		conf Config
+	)
+	// 1.把配置文件读进来
+	if content, err = ioutil.ReadFile(filename); err != nil {
+		return
+	}
+
+	// 2.做JSON反序列化
+	if err = json.Unmarshal(content, &conf); err != nil {
+		return
+	}
+
+	// 3.赋值单例
+	G_config = &conf
+
+	return
+}
