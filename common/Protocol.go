ba7e0fffb0b50a6b7f847b5b0bc5e8509f51c0ba
zuozhen.zuo666 <zuozhen.zuo666@bytedance.com>
2021-08-29 19:15:59 +0800

    master mongodb
    Change-Id: I2e93f33b21b35f72aaca7558489f70c24bbb7c4a

--- common/Protocol.go
+++ common/Protocol.go
@@ -48,14 +48,14 @@ type JobExecuteResult struct {
 
 // 任务执行日志
 type JobLog struct {
-	JobName      string `bson:"jobName"`      // 任务名字
-	Command      string `bson:"command"`      // 脚本命令
-	Err          string `bson:"err"`          // 错误原因
-	Output       string `bson:"output"`       // 脚本输出
-	PlanTime     int64  `bson:"planTime"`     // 计划开始时间
-	ScheduleTime int64  `bson:"scheduleTime"` // 实际调度时间
-	StartTime    int64  `bson:"startTime"`    // 任务执行开始时间
-	EndTime      int64  `bson:"endTime"`      // 任务执行结束时间
+	JobName      string `bson:"jobName" json:"jobName"`           // 任务名字
+	Command      string `bson:"command" json:"command"`           // 脚本命令
+	Err          string `bson:"err" json:"err"`                   // 错误原因
+	Output       string `bson:"output" json:"output"`             // 脚本输出
+	PlanTime     int64  `bson:"planTime" json:"planTime"`         // 计划开始时间
+	ScheduleTime int64  `bson:"scheduleTime" json:"scheduleTime"` // 实际调度时间
+	StartTime    int64  `bson:"startTime" json:"startTime"`       // 任务执行开始时间
+	EndTime      int64  `bson:"endTime" json:"endTime"`           // 任务执行结束时间
 }
 
 // 日志批次
